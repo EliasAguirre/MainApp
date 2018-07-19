24376ceecc3bb9b69e03915cbceebb296bc64ad6
eliasaguirre <eliasaguirrea97@gmail.com>
2018-07-19 13:30:58 -0500

    Images Uploaded Saved
    Files moved functional, saving uploaded pics to uploads folder

--- Server/controllers/profile-controller.js
+++ Server/controllers/profile-controller.js
@@ -2,6 +2,7 @@
 //and path(path creation)
 
 //injections
+//once this is done, create uploads folder to store these objects
 var User = require('../datasets/users.js');
 var fs = require('fs-extra');
 var path = require('path');
@@ -12,4 +13,24 @@ module.exports.updatePhoto = function(req, res){
 
     console.log("User " + userId + " is submitting", file);
 
+    //temp path of image uploaded, retrieved in terminal
+    //starting point for uploads folder
+    var tempPath = file.path;
+    //create unique date to identify unique path to uploads
+    var uploadDate = new Date().toISOString;
+
+    //clean up Date
+    // uploadDate = uploadDate.replace("." , "");
+    // uploadDate = uploadDate.replace("-" , "");
+    // uploadDate = uploadDate.replace(":" , "");
+
+    var targetPath = path.join(__dirname, "../../uploads/" + userId + uploadDate + file.name);
+
+    fs.rename(tempPath, targetPath, function(err){
+      if(err){
+        console.log(err);
+      } else{
+        console.log("file moved");
+      }
+    })
 }
